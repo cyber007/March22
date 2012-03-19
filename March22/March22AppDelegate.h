6ea379d8646a06e56a8517e46ef377df8d7754d1
admin <admin@admins-MacBook-Air.local>
2012-03-19 12:26:05 -0400

    Initial Commit

---
+++ March22/March22AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  March22AppDelegate.h
+//  March22
+//
+//  Created by admin on 3/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface March22AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
